2e8c37cdd12f586561f090b0f777b0dc7223ada4
Rob George <rob@robgeorge.co.uk>
2020-05-22 09:18:06 +0100

    Simple title update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bootcamp rules!
                 </div>
 
                 <div class="links">
